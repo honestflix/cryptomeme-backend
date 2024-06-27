6568ce3fef4850be255d8ca9d8c99c730a116025
honestflix <roninhara777@gmail.com>
2024-06-28 05:51:55 +0900

    feat: update main.py

--- app/exceptions.py
+++
@@ -1,9 +0,0 @@
-from fastapi.responses import JSONResponse
-from fastapi.requests import Request
-from fastapi import HTTPException
-
-async def custom_http_exception_handler(request: Request, exc: HTTPException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"message": exc.detail},
-    )
